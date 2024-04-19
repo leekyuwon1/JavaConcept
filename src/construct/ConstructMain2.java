33dcfd7cf9cb2ce5a3b2be789ec9b32c72f51819
leekyuwon <sjin9574@naver.com>
2024-04-19 23:37:50 +0900

    240419

--- src/construct/ConstructMain2.java
+++ src/construct/ConstructMain2.java
@@ -1,11 +1,11 @@
 package construct;
 
-public class ConstructMain1 {
+public class ConstructMain2 {
 
     public static void main(String[] args) {
         // 멤버 필드가 메모리에 할당한 뒤 인스턴스 생성 후 즉시 생성자를 불러온다.
         MemberConstruct member1 = new MemberConstruct("user1", 15, 90);
-        MemberConstruct member2 = new MemberConstruct("user2", 16, 80);
+        MemberConstruct member2 = new MemberConstruct("user2", 16);
 
         MemberConstruct[] members = {member1, member2};
         for (MemberConstruct s : members) {
