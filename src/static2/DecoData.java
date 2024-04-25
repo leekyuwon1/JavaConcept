56796010e1de669cc4430c86ad71ab87b179f346
leekyuwon <sjin9574@naver.com>
2024-04-25 23:32:00 +0900

    240425

---
+++ src/static2/DecoData.java
@@ -0,0 +1,33 @@
+package static2;
+
+public class DecoData {
+
+    private int instanceValue;
+    private static int staticValue;
+
+    public static void staticCall() {
+//        instanceValue++; // 인스턴스 변수 접근, compile error
+//        instanceMethod(); // 인스턴스 메서드 접근, compile error
+
+        /**
+         * static 은 클래스 소속, 하지만 인스턴스 변수, 인스턴스 메서드는 객체가 생성되어서 메모리에 할당이 되어야 참조값으로 찾아갈 수 있다.
+         * */
+        staticValue++; // 정적 변수 접근
+        staticMethod(); // 정적 메서드 접근
+    }
+
+    public void instanceCall() {
+        instanceValue++; // 인스턴스 변수 접근
+        instanceMethod(); // 인스턴스 메서드 접근
+
+        staticValue++; // 클래스 변수
+        staticMethod(); // 클래스 메서드
+    }
+    private void instanceMethod() {
+        System.out.println("instanceMethod " + instanceValue);
+    }
+
+    private static void staticMethod() {
+        System.out.println("staticValue " + staticValue);
+    }
+}
---
+++ src/static2/DecoMain2.java
@@ -0,0 +1,14 @@
+package static2;
+
+public class DecoMain2 {
+
+    public static void main(String[] args) {
+        String s = "hello java";
+        String deco = DecoUtil2.deco(s);
+
+
+        System.out.println("Before deco: " + s);
+        System.out.println("After deco: " + deco);
+
+    }
+}
