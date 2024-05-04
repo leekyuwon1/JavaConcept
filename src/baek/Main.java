e86cde62464fa313919b395b52f07658a2244f81
leekyuwon <sjin9574@naver.com>
2024-05-04 22:35:55 +0900

    240425

--- src/baek/Main.java
+++ src/baek/Main.java
@@ -1,18 +1,11 @@
 package baek;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        Scanner sc = new Scanner(System.in);
-        long a = br.read();
-        long b = br.read();
-        long c = br.read();
-
-        System.out.println(a + b + c);
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
     }
 }
