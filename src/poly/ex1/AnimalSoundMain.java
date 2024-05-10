712c1d511553eadc85d150647e80b15a043bacf6
leekyuwon <sjin9574@naver.com>
2024-05-10 23:59:57 +0900

    240425

---
+++ src/poly/ex1/AnimalSoundMain.java
@@ -0,0 +1,29 @@
+package poly.ex1;
+
+import java.util.ArrayList;
+
+public class AnimalSoundMain {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+
+        // 중복 코드제거가 안된다.
+        System.out.println("동물 소리 테스트 시작");
+        dog.sound();
+        System.out.println("동물 소리 테스트 종료");
+
+        System.out.println("동물 소리 테스트 시작");
+        cat.sound();
+        System.out.println("동물 소리 테스트 종료");
+
+        soundCaw(caw);
+    }
+
+    private static void soundCaw(Caw caw) {
+        System.out.println("동물 소리 테스트 시작");
+        caw.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
