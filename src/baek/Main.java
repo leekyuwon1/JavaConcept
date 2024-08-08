c8a5f7846a536125003fbe4b7e71fa29f788a819
leekyuwon <sjin9574@naver.com>
2024-08-08 23:56:07 +0900

    240425

--- src/baek/Main.java
+++ src/baek/Main.java
@@ -1,19 +1,49 @@
 package baek;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] array = sc.nextLine().split(" ");
+        int n = sc.nextInt();
+        int a = 0;
+        for (int i = 0; i < n; i++) {
 
-        while (sc.hasNext()) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println(a + b);
         }
 
-        sc.close();
+        String s = sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            if (array[i] == s) {
+                a++;
+            }
+        }
     }
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int n = Integer.parseInt(st.nextToken());
+//        int[] array = new int[n];
+//        int a = 0;
+//
+//        for (int i = 0; i < n; i++) {
+//            StringTokenizer in = new StringTokenizer(br.readLine(), " ");
+//            array[i] = Integer.parseInt(in.nextToken());
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//            StringTokenizer in1 = new StringTokenizer(br.readLine(), " ");
+//            if (array[i] == Integer.parseInt(in1.nextToken())) {
+//                a++;
+//            }
+//        }
+//        System.out.println(a);
+//    }
 }
 
