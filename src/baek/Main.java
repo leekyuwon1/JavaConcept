1ab5d586fe77f085a050faff7e8ac6fb80edb2ad
leekyuwon <sjin9574@naver.com>
2024-08-09 23:35:23 +0900

    240425

--- src/baek/Main.java
+++ src/baek/Main.java
@@ -3,47 +3,65 @@ package baek;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] array = sc.nextLine().split(" ");
-        int n = sc.nextInt();
-        int a = 0;
-        for (int i = 0; i < n; i++) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        }
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        String s = sc.nextLine();
-        for (int i = 0; i < n; i++) {
-            if (array[i] == s) {
-                a++;
+        String[] stLength = new String[Integer.parseInt(st.nextToken())];
+        int N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            if (st.nextToken() != null) {
+                stLength[i] = st.nextToken();
             }
         }
+
+        for (int i = 0; i < stLength.length; i++){
+            if (N < Integer.parseInt(stLength[i])) {
+                stLength[i] = stLength[i] + " ";
+            }
+        }
+        System.out.println(Arrays.toString(stLength));
     }
 
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-//        int n = Integer.parseInt(st.nextToken());
-//        int[] array = new int[n];
-//        int a = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String[] arrays = new String[sc.nextInt()];
+//        int a = sc.nextInt();
 //
-//        for (int i = 0; i < n; i++) {
-//            StringTokenizer in = new StringTokenizer(br.readLine(), " ");
-//            array[i] = Integer.parseInt(in.nextToken());
+//        for (int i = 0; i < arrays.length; i++){
+//            arrays[i] = sc.next();
 //        }
 //
-//        for (int i = 0; i < n; i++) {
-//            StringTokenizer in1 = new StringTokenizer(br.readLine(), " ");
-//            if (array[i] == Integer.parseInt(in1.nextToken())) {
-//                a++;
+//
+//        for (int i = 0; i < arrays.length; i++){
+//            if (a < Integer.parseInt(arrays[i])) {
+//                arrays[i] = arrays[i] + " ";
 //            }
 //        }
-//        System.out.println(a);
+//        System.out.println(Arrays.toString(arrays));
 //    }
 }
 
